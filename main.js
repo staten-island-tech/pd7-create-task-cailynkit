18c1c8bb973d2c272891570a55ae9fe3950f9935
cailynkit <cailynkitchens8@gmail.com>
2023-02-16 14:02:11 -0500

    eh

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const DOMSelectors = {
   input: document.getElementById("input"),
   number: document.querySelectorAll(".number"),
   operation: document.querySelectorAll(".operation"),
+  delete: document.getElementById("delete"),
   clear: document.getElementById("clear"),
 };
 
@@ -11,6 +12,21 @@ DOMSelectors.number.forEach((button) => {
   });
 });
 
+DOMSelectors.operation.forEach((button) => {
+  button.addEventListener("click", function () {
+    let number1 = DOMSelectors.input.innerHTML;
+    console.log(number1);
+    DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+  });
+});
+
+DOMSelectors.delete.addEventListener("click", function () {
+  let newNumber = DOMSelectors.input.innerHTML.slice(0, -1);
+  DOMSelectors.input.innerHTML = `${newNumber}`;
+});
+
 DOMSelectors.clear.addEventListener("click", function () {
   DOMSelectors.input.innerHTML = "";
 });
+
+DOMSelectors.enter.addEventListener("click", function () {});
