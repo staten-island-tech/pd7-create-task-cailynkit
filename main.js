55a9c04bb87f57cfbc29708148c5208388e69081
cailynkit <cailynkitchens8@gmail.com>
2023-02-17 14:02:25 -0500

    sucks

--- main.js
+++ main.js
@@ -4,18 +4,58 @@ const DOMSelectors = {
   operation: document.querySelectorAll(".operation"),
   delete: document.getElementById("delete"),
   clear: document.getElementById("clear"),
+  enter: document.getElementById("enter"),
 };
+
+let number1;
+let number2;
+let operation;
 
 DOMSelectors.number.forEach((button) => {
   button.addEventListener("click", function () {
-    DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+    if (
+      DOMSelectors.input.innerHTML.includes("+") === false &&
+      DOMSelectors.input.innerHTML.includes("-") === false &&
+      DOMSelectors.input.innerHTML.includes("*") === false &&
+      DOMSelectors.input.innerHTML.includes("/") === false
+    ) {
+      DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+      number1 = DOMSelectors.input.innerHTML;
+      console.log(number1);
+    } else if (
+      DOMSelectors.input.innerHTML.includes("+") ||
+      DOMSelectors.input.innerHTML.includes("-") ||
+      DOMSelectors.input.innerHTML.includes("*") ||
+      DOMSelectors.input.innerHTML.includes("/")
+    ) {
+      DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+      number2 = (number2 || "") + button.innerHTML;
+      console.log(number2);
+    }
   });
 });
 
+/* DOMSelectors.number.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (
+      DOMSelectors.input.innerHTML.includes("+") ||
+      DOMSelectors.input.innerHTML.includes("-") ||
+      DOMSelectors.input.innerHTML.includes("*") ||
+      DOMSelectors.input.innerHTML.includes("/")
+    ) {
+      DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+      number2 = DOMSelectors.input.innerHTML;
+      console.log(number2);
+    } else {
+      DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
+      number1 = DOMSelectors.input.innerHTML;
+      console.log(number1);
+    }
+  });
+}); */
+
 DOMSelectors.operation.forEach((button) => {
   button.addEventListener("click", function () {
-    let number1 = DOMSelectors.input.innerHTML;
-    console.log(number1);
     DOMSelectors.input.insertAdjacentHTML("beforeend", `${button.innerHTML}`);
   });
 });
@@ -29,4 +69,10 @@ DOMSelectors.clear.addEventListener("click", function () {
   DOMSelectors.input.innerHTML = "";
 });
 
-DOMSelectors.enter.addEventListener("click", function () {});
+DOMSelectors.enter.addEventListener("click", function () {
+  if (DOMSelectors.input.innerHTML.includes("+")) {
+    let sum = number1 + number2;
+    console.log(sum);
+    DOMSelectors.input.innerHTML = `${sum}`;
+  }
+});
